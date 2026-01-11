fa4f196324410a172666fd5aa60b126e3ee182ce
leodelarocka <>
2026-01-11 18:10:42 +0000

    While Loops

---
+++ la_os_de_repeti_o/while_loops/main.py
@@ -0,0 +1,11 @@
+start_number = 5
+countdown_values = []
+
+# Countdown usando while
+while start_number >= 1:
+    countdown_values.append(start_number)
+    start_number -= 1
+
+# Exibe resultado após o loop
+print("Discount countdown complete!")
+print(countdown_values)
