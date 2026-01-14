ea3a1c58d8daef404b8e7d5f58a1a19c0326c0b1
leodelarocka <>
2026-01-14 16:59:17 +0000

    Nested loops

--- la_os_de_repeti_o/nested_loops/main.py
+++ la_os_de_repeti_o/nested_loops/main.py
@@ -1,7 +1,8 @@
-produce = ["Tomatoes", "Lettuce"]
-dairy = ["Milk", "Cheese"]
+#Listas Aninhadas - Nested lists
+produce = ["Tomatoes", "Lettuce"] # Cria uma lista chamada produce com frutas/vegetais.
+dairy = ["Milk", "Cheese"] # Cria uma lista chamada produce com frutas/vegetais.
 
-groceries = [produce, dairy]
-for section in groceries:
-    for item in section:
-        print(f"Item name: {item}")
+groceries = [produce, dairy] # Combina as duas listas numa lista maior chamada groceries. Agora groceries é uma lista de duas sublistas.
+for section in groceries: # Inicia o laço externo: em cada iteração, section recebe uma das sublistas (produce na 1ª vez, dairy na 2ª).
+    for item in section: # Laço interno: percorre cada elemento dentro da sublista atual.
+        print(f"Item name: {item}") # Imprime a lista baseada na variavel temporaria aninhada "item" 
