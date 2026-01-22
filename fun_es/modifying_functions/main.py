23f30007b4567bfdd05c6d91f15281b436e7ab01
leodelarocka <>
2026-01-22 19:24:08 +0000

    Modifying Functions

--- fun_es/modifying_functions/main.py
+++ fun_es/modifying_functions/main.py
@@ -1,19 +1,23 @@
 
-
+#Funcao que aplica desconto
 def apply_discount(price, discount=0.05):
     discounted_price = price * (1 - discount)
     return discounted_price
     
+#Funcao que aplica taxas    
 def apply_tax(price, tax=0.07):
     return price * (1 + tax)
-    
+
+#Funcao que calcula o total    
 def calculate_total(price, discount=0.05, tax=0.07):
     discounted = apply_discount(price, discount)
     total = apply_tax(discounted, tax)
     return total
 
+# Chamada com valores padrão
 total_default = calculate_total(120)
-print(f"Total cost with custom discount and tax: ${total_default}")
+print(f"Total cost with default discount and tax: ${total_default}")
 
+# Chamada com valores customizados
 total_custom = calculate_total(100, discount=0.10, tax=0.08)
 print(f"Total cost with custom discount and tax: ${total_custom}")
